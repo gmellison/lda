f693db207a53375c476b8d38e0aeb2d7a4313d22
Greg Ellison <gmhellison@gmail.com>
2017-01-15 14:56:55 -0800

    adding matrix tests

---
+++ src/test/scala/matrix/MatrixTests.scala
@@ -0,0 +1,34 @@
+package org.greg.wine
+
+import org.scalatest.FunSuite
+import org.greg.wine.matrix._
+
+class MatrixTests extends FunSuite {
+  
+  assertThrows[java.lang.AssertionError] {
+    new Matrix(List())
+  } 
+  
+  assertThrows[java.lang.AssertionError] {
+    new Matrix(List(List()))
+  }
+  
+  // Matrix tests
+  val mat1 = new Matrix(List(List(0.0, 1.0), List(2.0, 3.0)))
+  val mat1t = new Matrix(List(List(0.0, 2.0), List(1.0, 3.0)))
+
+  assert(mat1.transpose() == mat1t)
+  assert(mat1.determinant() == -2.0)
+
+  val mat2 = new Matrix(List(List(0.0, 1.0)))
+
+  assertThrows[java.lang.AssertionError] {
+    mat2.inverse()
+  }
+
+  // Identity matrix tests
+  val i2 = Identity(2)
+
+  assert(i2 == new Matrix(List(List(1.0, 0.0), List(0.0, 1.0))))
+  
+}
