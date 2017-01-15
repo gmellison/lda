896e992a945b9c8807c4a0395a5d6f52bf126f44
Greg Ellison <gmhellison@gmail.com>
2017-01-15 11:51:20 -0800

    some changes to distibution classes

--- src/main/scala/random/Normal.scala
+++ src/main/scala/random/Normal.scala
@@ -6,38 +6,51 @@ import scala.math.log
 import scala.math.exp
 import scala.math.Pi
 import scala.math.pow
-//import org.greg.wine.matrix.Matrix
+import org.greg.wine.matrix._
 
-class Normal(mean: Double = 0, variance: Double = 1) {
+class Normal(m: Double = 0.0, v: Double = 1.0) {
 
-  assert(variance >= 0)
+  assert(v >= 0)
+
+  val mean = m
+  val variance = v
 
   def pdf(x: Double): Double = {
-    (1 / sqrt(2 * Pi * this.variance)) * exp( (1 / 2 * variance) * pow((x - this.mean), 2) )
+    val coef = (1.0 / sqrt(2.0 * Pi * this.variance))
+    val exp_arg = (-1.0 / (2 * this.variance)) * pow((x - this.mean), 2) 
+    coef * exp(exp_arg)
   }
+  
    
   override def toString() = {
     s"~ Normal(mean = $mean, variance = $variance)"
   }
 }
 
-/*class MultiNormal(mean: Matrix, covariance: Matrix) {*/
-  //assert(mean.nrow == 1)
-  //assert(mean.ncol > 0)
-  //assert(covariance.ncol == covariance.ncol)
+class MultiNormal(m: Matrix, cov: Matrix) {
+
+  val covariance = cov
+  val mean = m
+  val k = cov.ncol
+
+  assert(this.mean.nrow == 1)
+  assert(this.mean.ncol > 0)
+  assert(this.covariance.nrow == this.covariance.ncol)
   
-  //val k = covariance.ncol
+  def pdf(x: Matrix): Double = {
 
-  //def pdf(x: Matrix): Double = {
-
-    //1.0    
-///*    assert(x.nrow == 1)<]*/
-    ////assert(x.ncol == mean.ncol)
+    assert(x.nrow == 1)
+    assert(x.ncol == this.mean.ncol)
  
-    ////val inv_cov = covariance.inverse().determinant()
-    ////val coef = inv_cov.scalar_mult(1 / pow(2 * Pi)) 
-    ////val exp_arg: Double = (-1.0 / 2.0)
+    val cov_inv = this.covariance.inverse()
+    val cov_det = this.covariance.determinant()
+    val coef = (1 / sqrt(cov_det * pow(2 * Pi, k)))
+    val res = x.subtract(this.mean)
+    val exp_arg: Double = res.matrix_mult(cov_inv).
+      matrix_mult(res.transpose()).
+      scalar_mult(- 0.5).
+      matrix(0)(0)
 
-
-  //}
-/*}*/
+    coef * exp(exp_arg)
+  }
+}
